d79fbf089cfbcb3a02d92213abb219e9b05fa6e2
Gietter <70861979+Gietter@users.noreply.github.com>
2021-12-07 16:06:39 -0300

    Delete Funcionário/src directory

--- Funcionário/src/main/java/br/com/sistema/repository/ProjetoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sistema.repository;
-
-import br.com.sistema.model.Projeto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
-
-
-}
